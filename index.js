f100ef052b280437693afb04daea701e02269f99
Claire Nguyen <disarrangedorder@gmail.com>
2018-11-11 21:57:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})
+
+it('dec(n) decrements n and returns the result', function() {
+  expect(dec(a)).toEqual(a - 1)
+})
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+  */
+
+  inc(2)
+  
